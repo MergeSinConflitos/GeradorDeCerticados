0b9e88173cb75dbfbc73c5496a7eedcf6bb6a16f
Igor Mota <igormello399@gmail.com>
2026-09-18 16:07:20 -0300

    Removidos metodos obsoletos de ErrosDeUsuario

--- src/Aplicacao/Usuarios/Util/ErrosDeUsuario.cs
+++ src/Aplicacao/Usuarios/Util/ErrosDeUsuario.cs
@@ -12,30 +12,6 @@ public static class ErrosDeUsuario
             "Email ou senha inválidos."
         );
 
-    public static Error EmailInvalido()
-        => new Error("O email informado é inválido.")
-            .WithMetadata("Codigo", "Usuario.EmailInvalido");
-
-    public static Error EmailJaCadastrado()
-        => new Error("O email informado já está cadastrado.")
-            .WithMetadata("Codigo", "Usuario.EmailJaCadastrado");
-
-    public static Error SenhaMuitoCurta()
-        => new Error("A senha deve possuir no mínimo 8 caracteres.")
-            .WithMetadata("Codigo", "Usuario.SenhaMuitoCurta");
-
-    public static Error SenhaSemDigito()
-        => new Error("A senha deve conter pelo menos um dígito.")
-            .WithMetadata("Codigo", "Usuario.SenhaSemDigito");
-
-    public static Error SenhaSemCaractereNaoAlfanumerico()
-        => new Error(
-            "A senha deve conter pelo menos um caractere não alfanumérico."
-        )
-        .WithMetadata(
-            "Codigo",
-            "Usuario.SenhaSemCaractereNaoAlfanumerico"
-        );
 
     internal static IError ConflitoDeIdentidade(
         string mensagem
